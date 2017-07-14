8b9db2ccb2e8b1f5439490d73a76d789a7a4eb53
kanishka-linux <kanishka.linux@gmail.com>
2017-07-14 18:44:21 +0530

    add launcher

--- hlspy/__init__.py
+++ hlspy/__init__.py
@@ -19,6 +19,7 @@ along with hlspy.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 import os
+import subprocess
 
 if getattr(sys,'frozen',False):
 	BASEDIR,BASEFILE = os.path.split(os.path.abspath(sys.executable))
@@ -31,6 +32,14 @@ sys.path.insert(0,BASEDIR)
 
 from PyQt5 import QtWidgets
 from hls import BrowseUrlT
+
+def define_path(file_name):
+	hd,tl = os.path.split(file_name)
+	file_path = os.path.join(os.getcwd(),tl)
+	if hd:
+		if os.path.exists(hd) and tl:
+			file_path = file_name
+	return file_path
 
 def main():
 	app = QtWidgets.QApplication(sys.argv)
@@ -45,6 +54,15 @@ def main():
 		'--timeout=IN_SECONDS','--block-request=','--show-window',
 		'--show-window=wxh','--grab-window=image.png','--print-pdf=web.pdf'
 		]
+	launcher_template = """
+#!/usr/bin/env xdg-open
+[Desktop Entry]
+Type=Application
+Name=
+Exec=
+Terminal=false
+NoDisplay=false
+"""
 	home = os.path.join(os.path.expanduser('~'),'.config','hlspy')
 	if not os.path.exists(home):
 		os.makedirs(home)
@@ -57,7 +75,7 @@ def main():
 	show_window = False
 	grab_window = None
 	timeout = 0
-	for  i in sys.argv:
+	for  j,i in enumerate(sys.argv):
 		if i.startswith('--set-cookie-file='):
 			set_cookie = i.split('=')[1]
 		elif i.startswith('--use-cookie-file='):
@@ -75,6 +93,9 @@ def main():
 		elif i.startswith('--output='):
 			print(i.split('='))
 			out_file = i.split('=')[1]
+			if out_file:
+				if out_file.lower() == 'false' or out_file.lower() == 'true':
+					out_file = False
 		elif i.startswith('--wait-for-cookie'):
 			wait_for_cookie = True
 		elif i.startswith('--print-request'):
@@ -90,7 +111,14 @@ def main():
 		elif i.startswith('--show-window'):
 			show_window = True
 			if '=' in i:
-				window_dim = i.split('=')[1]
+				window_val = i.split('=')[1]
+				window_dim = '500'
+				if window_val.lower() == 'true':
+					show_window = True
+				elif window_val.lower() == 'false':
+					show_window = False
+				else:
+					window_dim = window_val
 		elif i.startswith('--select-request='):
 			select_request = i.split('=')[1]
 		elif i.startswith('--grab-window'):
@@ -105,29 +133,67 @@ def main():
 			for i in option_arr:
 				print(i)
 			sys.exit(0)
+		elif i.startswith('--create-launcher='):
+			launcher_name = i.split('=')[1]
+			launcher_exec = ''
+			for k,l in enumerate(sys.argv):
+				#print(k,l)
+				if k == 0:
+					if l == '__init__.py':
+						launcher_exec = 'hlspy'
+					else:
+						launcher_exec = l
+				else:
+					if not l.startswith('--create-launcher='):
+						launcher_exec = launcher_exec + ' ' + l
+			#print(launcher_exec)
+			edit_template = launcher_template.replace("Name=","Name="+launcher_name)
+			edit_template = edit_template.replace("Exec=","Exec="+launcher_exec)
+			if launcher_name and launcher_exec:
+				launch_home = os.path.join(home,launcher_name+'.desktop')
+				f = open(launch_home,'wb')
+				f.write(edit_template.encode('utf-8'))
+				f.close()
+		elif i.startswith('--launch'):
+			launcher_name = os.path.join(home,sys.argv[j+1] + '.desktop')
+			if os.path.isfile(launcher_name):
+				if sys.platform == 'linux':
+					subprocess.Popen(['xdg-open',launcher_name])
+					sys.exit(0)
+				else:
+					f = open(launcher_name,'r')
+					lines = f.readlines()
+					f.close()
+					for l in lines:
+						if l.startswith('Exec='):
+							cmd = (l.replace('Exec=','',1).strip()).split(' ')
+							print(cmd,'--170--')
+							subprocess.Popen(cmd)
+							sys.exit(0)
+			
 	#print(url)
 	#print(sys.argv)
 	if t_dir is None:
 		t_dir = home
-	if set_cookie is None:
-		set_cookie = os.path.join(home,'cookie.txt')
-	else:
-		set_cookie = os.path.join(os.getcwd(),set_cookie)
+	if set_cookie is not None:
+		set_cookie = define_path(set_cookie)
+		if os.path.isfile(set_cookie):
+			os.remove(set_cookie)
 		
 	if use_cookie:
-		use_cookie = os.path.join(os.getcwd(),use_cookie)
+		use_cookie = define_path(use_cookie)
 	
 	if js_file:
-		js_file = os.path.join(os.getcwd(),js_file)
+		js_file = define_path(js_file)
 	
 	if out_file:
-		out_file = os.path.join(os.getcwd(),out_file)
+		out_file = define_path(out_file)
 	
 	if print_pdf:
-		print_pdf = os.path.join(os.getcwd(),print_pdf)
+		print_pdf = define_path(print_pdf)
 	
 	if grab_window:
-		grab_window = os.path.join(os.getcwd(),grab_window)
+		grab_window = define_path(grab_window)
 	
 	if domain_name is None:
 		if url.startswith('http'):
@@ -139,8 +205,6 @@ def main():
 	if user_agent is None:
 		user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:45.0) Gecko/20100101 Firefox/45.0'
 		
-	if os.path.isfile(set_cookie):
-		os.remove(set_cookie)
 		
 	web = BrowseUrlT(url,set_cookie=set_cookie,use_cookie=use_cookie,
 			end_point=end_point,domain_name=domain_name,user_agent=user_agent,
